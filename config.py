c87b5b69f175b8cb64d74f9490e01c85dcf9309f
kebo <itachi971009@gmail.com>
2021-01-18 00:37:01 +0800

    [fea] add config

---
+++ config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+这一行开始写关于本文件的说明与解释
+
+
+'''
+# token config
+ALGORITHM = "HS256"
+SECRET_KEY = ""
+ACCESS_TOKEN_EXPIRE_DAYS = 7
+
+# Database config
+SQLALCHEMY_DATABASE_URL = ""
